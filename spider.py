88e7d4c2f2a90312ded80269fa6a0ce5fb619660
EtienneDG <etienne.dussert@goood.pro>
2016-10-28 14:51:22 +0200

    Adding gitignore

---
+++ spider.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+
+def get_page_content(url):
+    try:
+        return requests.get(url)
+    except:
+        return "Error :("
+
+
+def get_soup(text):
+    return bs.BeautifulSoup(text)
+
+login_url = "https://www.materiel.net/pm/client/login.html"
+html = get_page_content(login_url)
+soup = get_soup(html)
+
+loginInput = ""
